c173b03bb5c1b93104ed24ec1fa09e52785238b1
Shashank Jarmale <shashankjarmale1@gmail.com>
2022-12-04 00:20:38 -0500

    Day 4

--- day4/p2-solution.py
+++ day4/p2-solution.py
@@ -4,10 +4,27 @@ def loadInput():
     return lines
 
 
+def overlap(start1, start2, end1, end2):
+    if start1 >= start2 and end2 >= start1:
+        return True
+    elif start2 >= start1 and end1 >= start2:
+        return True
+
+    return False
+
+
 def main():
     lines = loadInput()
+    count = 0
     for line in lines:
-        print(line)
+        pair = line.split(",")
+        firstElf = pair[0].split("-")
+        secondElf = pair[1].split("-")
+
+        if overlap(int(firstElf[0]), int(secondElf[0]), int(firstElf[1]), int(secondElf[1])):
+            count += 1
+
+    print(count)
 
 
 main()
